19eb16296bd89ad6a679ff496de8b1157018101c
Okegbe AKpofure Kelvin-Faith <okegbeakpofurekelvin@gmail.com>
2023-10-13 22:52:51 +0100

    integrated to render url

--- public/javascript/book.js
+++ public/javascript/book.js
@@ -1,77 +1,96 @@
 import axios from "axios";
 import { showAlert } from "./alerts";
 
-export const addBook = async (Title, DatePublished, Description, PageCount, Genre, BookImage, BookLandscapeImages, Summary, Publisher)=>{
-    try{
-        const res = await axios({
-            method: 'POST',
-            url: 'http://127.0.0.1:3000/books',
-            data: {
-                Title,
-                DatePublished,
-                Description,
-                PageCount,
-                Genre,
-                BookImage,
-                BookLandscapeImages,
-                Summary,
-                Publisher
-            }
-        })
-        if(res.statusText === 'Created'){
-            showAlert('success','Book added successfully')
-            window.setTimeout(() => {
-                location.assign('/api/my-books')
-            }, 1500)
-        }
-    }catch(error){
-        showAlert('error',error.response.data.message)
+export const addBook = async (
+  Title,
+  DatePublished,
+  Description,
+  PageCount,
+  Genre,
+  BookImage,
+  BookLandscapeImages,
+  Summary,
+  Publisher
+) => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "https://akpofures-library-gdql.onrender.com/books",
+      data: {
+        Title,
+        DatePublished,
+        Description,
+        PageCount,
+        Genre,
+        BookImage,
+        BookLandscapeImages,
+        Summary,
+        Publisher,
+      },
+    });
+    if (res.statusText === "Created") {
+      showAlert("success", "Book added successfully");
+      window.setTimeout(() => {
+        location.assign("/api/my-books");
+      }, 1500);
     }
-}
+  } catch (error) {
+    showAlert("error", error.response.data.message);
+  }
+};
 
-export const deleteBook = async (BookID)=>{
-    try{
-        const res = await axios({
-            method: 'DELETE',
-            url: `http://127.0.0.1:3000/books/delete/${BookID}`,
-            
-        })
-        if(res.statusText === 'OK'){
-            showAlert('success','Book deleted successfully')
-            window.setTimeout(() => {
-                location.assign('/api/my-books')
-            }, 1500)
-        }
-    }catch(error){
-        showAlert('error',error.response.data.error)
+export const deleteBook = async (BookID) => {
+  try {
+    const res = await axios({
+      method: "DELETE",
+      url: `https://akpofures-library-gdql.onrender.com/books/delete/${BookID}`,
+    });
+    if (res.statusText === "OK") {
+      showAlert("success", "Book deleted successfully");
+      window.setTimeout(() => {
+        location.assign("/api/my-books");
+      }, 1500);
     }
-}
+  } catch (error) {
+    showAlert("error", error.response.data.error);
+  }
+};
 
-export const updateBook = async (BookID, Title, DatePublished, Description, PageCount, Genre, BookImage, BookLandscapeImages, Summary, Publisher)=>{
-    try{
-        const res = await axios({
-            method: 'PUT',
-            url: `http://127.0.0.1:3000/books/update/${BookID}`,
-            data: {
-                Title,
-                DatePublished,
-                Description,
-                PageCount,
-                Genre,
-                BookImage,
-                BookLandscapeImages,
-                Summary,
-                Publisher
-            }
-        })
-        if(res.statusText === 'OK'){
-            showAlert('success','Book updated successfully')
-            window.setTimeout(() => {
-                location.assign(`/api/book/${BookID}`)
-            }, 1500)
-        }
-    }catch(error){
-        showAlert('error',error.response.data.error)
+export const updateBook = async (
+  BookID,
+  Title,
+  DatePublished,
+  Description,
+  PageCount,
+  Genre,
+  BookImage,
+  BookLandscapeImages,
+  Summary,
+  Publisher
+) => {
+  try {
+    const res = await axios({
+      method: "PUT",
+      url: `https://akpofures-library-gdql.onrender.com/books/update/${BookID}`,
+      data: {
+        Title,
+        DatePublished,
+        Description,
+        PageCount,
+        Genre,
+        BookImage,
+        BookLandscapeImages,
+        Summary,
+        Publisher,
+      },
+    });
+    if (res.statusText === "OK") {
+      showAlert("success", "Book updated successfully");
+      window.setTimeout(() => {
+        location.assign(`/api/book/${BookID}`);
+      }, 1500);
     }
-}
-        
+  } catch (error) {
+    showAlert("error", error.response.data.error);
+  }
+};
--- public/javascript/updateSettings.js
+++ public/javascript/updateSettings.js
@@ -1,26 +1,24 @@
-import axios from "axios"
-import { showAlert } from "./alerts"
+import axios from "axios";
+import { showAlert } from "./alerts";
 
-export const updateData = async(AuthorName, Email, PhoneNumber)=>{
-    try{
-        const res = await axios({
-            method: 'PUT',
-            url: 'http://127.0.0.1:3000/authors/updateAccount',
-            data: {
-                AuthorName,
-                Email,
-                PhoneNumber
-            }
-        })
-        if(res.statusText === 'OK'){
-            showAlert('success','Data updated successfully')
-            window.setTimeout(() => {
-                location.assign('/api/me')
-            }, 1500)
-        }
-    }catch(error){
-        showAlert('error', error.response.data.message)
-    
-}
-}
-     
+export const updateData = async (AuthorName, Email, PhoneNumber) => {
+  try {
+    const res = await axios({
+      method: "PUT",
+      url: "https://akpofures-library-gdql.onrender.com/authors/updateAccount",
+      data: {
+        AuthorName,
+        Email,
+        PhoneNumber,
+      },
+    });
+    if (res.statusText === "OK") {
+      showAlert("success", "Data updated successfully");
+      window.setTimeout(() => {
+        location.assign("/api/me");
+      }, 1500);
+    }
+  } catch (error) {
+    showAlert("error", error.response.data.message);
+  }
+};
